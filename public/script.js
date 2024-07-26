65d3c034cc07edcf98b34cdf6b4453d521a9604f
Ibrahim <oyesolaibrahim14@gmail.com>
2024-07-26 15:14:34 +0100

    first commit

--- public/script.js
+++ public/script.js
@@ -13,3 +13,21 @@ const menuOpen = () => {
     hamburger.style.opacity = "1";
   }
 };
+
+document.getElementById('shortenButton').addEventListener('click', function() {
+  var input = document.getElementById('urlInput').value;
+  var output = document.getElementById('shortenedUrl');
+  
+  if (input) {
+      var shortenedUrl = shortenUrl(input);
+      output.textContent = 'Shortened URL: ' + shortenedUrl;
+      output.style.color = 'white'; 
+  } else {
+      output.textContent = 'Please enter a URL to shorten';
+      output.style.color = 'red';  
+  }
+});
+function shortenUrl(url) {
+  return url.length > 15 ? url.slice(0, 15) + '...' : url;
+}
+
